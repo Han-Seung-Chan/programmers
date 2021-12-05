343a8ed1af912070f5bd7cb5373775e854418931
seungchanHan <seungchanhan00@gmail.com>
2021-12-05 23:56:31 +0900

    프로그래머스 알고리즘

---
+++ 82612-price.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+  let answer = -1;
+  let total = 0;
+  for (let i = 0; i <= count; i++) {
+    total += price * i;
+  }
+  answer = total - money;
+  if (answer > 0) return answer;
+  else return 0;
+}
+console.log(solution(3, 20, 4));
