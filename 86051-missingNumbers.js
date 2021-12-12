fb6ea7d008ec1c645f78257a43448ef08ac13a2d
seungchanHan <seungchanhan00@gmail.com>
2021-12-13 02:22:38 +0900

    프로그래머스 알고리즘

---
+++ 86051-missingNumbers.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  let answer = 0;
+
+  for (let i = 0; i < 10; i++) {
+    if (numbers.indexOf(i) === -1) answer += i;
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 6, 7, 8, 0]));
+console.log(solution([5, 8, 4, 0, 6, 7, 9]));
