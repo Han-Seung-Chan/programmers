f1810b012c4027835796bbbe6616ea931f8ddfa9
seungchanHan <seungchanhan00@gmail.com>
2021-12-25 23:34:21 +0900

    프로그래머스 알고리즘

---
+++ 12918-stringHandling.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  if (!(s.length == 4 || s.length == 6)) return false;
+  else {
+    const tmp = s.split('');
+    for (let i = 0; i < tmp.length; i++) {
+      if (isNaN(tmp[i])) return false;
+    }
+  }
+  return true;
+}
+console.log(solution('a234'));
+console.log(solution('1234'));
